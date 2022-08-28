e839cf13b98b529baa7929039e75630c6570e8e4
Guilherme Eric <guilherme.eric@outlook.com>
2022-08-28 12:31:48 -0300

    first challenge

--- src/challenge.test.ts
+++ src/challenge.test.ts
@@ -1,14 +1,48 @@
 import Challenge from "./challenge";
+import {IncidentManager} from "./incident-manager";
+import axios from "axios";
+
+jest.mock('axios')
 
 describe('Challenge', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
     it('should be defined', function () {
         expect(Challenge).toBeDefined()
     });
 
     describe('when calling the operation service', () => {
         //testing the firstChallenge
-        it('test', () => {
-            throw new Error("Not implemented yet")
+        it('should return operation when status is 200', async () => {
+            (axios.post as jest.Mock).mockResolvedValueOnce({
+                status: 200,
+                data: {
+                    operation: 'insert'
+                }
+            });
+
+            const incidentManager = new IncidentManager();
+            const challenge = new Challenge(incidentManager);
+
+            const result = await challenge.firstChallenge('guilherme', 23);
+
+            expect(result).toEqual('insert');
+        })
+
+        it('should return undefined when status is not 200', async () => {
+            (axios.post as jest.Mock).mockResolvedValueOnce({
+                status: 500
+            });
+
+            const incidentManager = new IncidentManager();
+            const challenge = new Challenge(incidentManager);
+
+            const result = await challenge.firstChallenge('guilherme', 23);
+
+            expect(result).not.toBeDefined();
         })
     })
 
@@ -32,6 +66,4 @@ describe('Challenge', () => {
             })
         })
     })
-
-
 })
